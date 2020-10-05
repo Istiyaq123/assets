ac4ca089f7446248f900aed03a32af21cf74a9ba
Istiyaq123 <57604080+Istiyaq123@users.noreply.github.com>
2020-10-05 22:52:33 +0100

    Update video_id.js

--- script/video_id.js
+++ script/video_id.js
@@ -1,4 +1,5 @@
 var video_id = document.getElementById("video_id");
+var iframe = document.getElementById("iframevid");
 
 if (video_id.videoid == "6a2e99d408a5487093b52442fad20fbf"){
   iframe.src = "https://pkplayer.netlify.app/content/video/4953336d7f544f678a12270b176ea386/kG/oWxGq-WtFVtiAjubsbQ-yVTGnYYkNKDFCZJw0lC2s/m3_english.html";
