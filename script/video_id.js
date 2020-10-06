0201676a25c5235e72717eb41467d92b34e0c48c
Istiyaq123 <57604080+Istiyaq123@users.noreply.github.com>
2020-10-06 08:32:47 +0100

    Update video_id.js

--- script/video_id.js
+++ script/video_id.js
@@ -1,9 +1,9 @@
 var video_id = document.getElementById("video_id");
 var iframe = document.getElementById("iframevid");
 
-if (video_id.videoid == "6a2e99d408a5487093b52442fad20fbf"){
+if (video_id.videoid === "6a2e99d408a5487093b52442fad20fbf"){
   iframe.src = "https://pkplayer.netlify.app/content/video/4953336d7f544f678a12270b176ea386/kG/oWxGq-WtFVtiAjubsbQ-yVTGnYYkNKDFCZJw0lC2s/m3_english.html";
 };
-if (video_id.videoid == "be70fa462bf6474a8050fa62a6245583"){
+if (video_id.videoid === "be70fa462bf6474a8050fa62a6245583"){
   iframe.src = "https://pkplayer.netlify.app/content/video/4953336d7f544f678a12270b176ea386/kG/oWxGq-WtFVtiAjubsbQ-yVTGnYYkNKDFCZJw0lC2s/m3_english.html";
 };
